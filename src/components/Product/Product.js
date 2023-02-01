a12b89fde9365d0b2680825d8622164392185697
Błażej Kowol <kowol.blazej@gmail.com>
2023-02-01 10:47:35 +0100

    fix products & add state to product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,26 +1,32 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Product = props => {
+
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={props.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>Price: {props.basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active} sizes={props.sizes}>S</button></li>
+              <li><button type="button" className={styles.active}>S</button></li>
               <li><button type="button">M</button></li>
               <li><button type="button">L</button></li>
               <li><button type="button">XL</button></li>
@@ -42,5 +48,7 @@ const Product = props => {
     </article>
   )
 };
+
+Product.propTypes = {props: PropTypes.func.isRequired}
 
 export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -7,24 +7,12 @@ const Products = () => {
 
   return (
     <section>
-      {products.map(product => <Product key={product.id} id={product.id} name={product.name} title={product.title} colors={product.colors} sizes={product.sizes} basePrice={product.basePrice} />)}
+      <Product {...products[0]} />
+      <Product {...products[1]} />
     </section>
   );
 };
 
 export default Products;
 
-  /*<Product
-  id={products[0].id}
-  name={products[0].name}
-  title={products[0].title}
-  colors={products[0].colors}
-  sizes={products[0].sizes}
-  basePrice={products[0].basePrice} />
-  <Product
-  id={products[1].id}
-  name={products[1].name}
-  title={products[1].title}
-  colors={products[1].colors}
-  sizes={products[1].sizes}
-  basePrice={products[1].basePrice} />*/
+/*{products.map(product => <Product key={product.id} id={product.id} name={product.name} title={product.title} colors={product.colors} sizes={product.sizes} basePrice={product.basePrice} />)}*/
