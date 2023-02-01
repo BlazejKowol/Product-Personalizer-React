74f30e8feb58b003a5c54fc2875d38dda621e8e2
Błażej Kowol <kowol.blazej@gmail.com>
2023-02-01 18:46:26 +0100

    change colors & sizes code

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,11 +3,16 @@ import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import shortid from 'shortid';
 
 const Product = props => {
 
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
 
   return (
     <article className={styles.product}>
@@ -26,18 +31,18 @@ const Product = props => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes?.map(size => <li key={shortid()}>
+                <button type="button" className={size.name && currentSize === size.name ? styles.active : undefined}>
+                  {`${size.name}`}
+                  </button></li>)}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors?.map(color => <li key={shortid()}>
+                <button type="button" className={clsx(prepareColorClassName(color),
+                currentColor === color && styles.active)}></button></li>)}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -52,3 +57,19 @@ const Product = props => {
 Product.propTypes = {props: PropTypes.func.isRequired}
 
 export default Product;
+
+/*{currentSize.map(size => <li key={size.id}><button type="button" className={styles.active}>$'{currentSize}'</button></li>)}
+
+              <li><button type="button" className={styles.active}>S</button></li>
+              <li><button type="button">M</button></li>
+              <li><button type="button">L</button></li>
+              <li><button type="button">XL</button></li>
+
+              {props.colors?.map(color => <li key={shortId()}> <button type="button" className={clsx(prepareColorClassName(color), currentColor === color && styles.active)}></button></li>)}
+
+              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
+              <li><button type="button" className={clsx(styles.colorRed)} /></li>
+              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+
+*/ 
+
